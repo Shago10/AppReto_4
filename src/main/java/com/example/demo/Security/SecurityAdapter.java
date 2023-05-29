e4349026a77b4c74f91853543bb0db89d0f4cc7a
santiagofrancose@outlook.com <127808019+Shago10@users.noreply.github.com>
2023-05-28 19:57:17 -0500

    Reto4

---
+++ src/main/java/com/example/demo/Security/SecurityAdapter.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.Security;
+
+/*
+import org.apache.catalina.filters.Constants;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import static java.lang.Compiler.disable;
+
+/* 
+*
+ * @author santi
+ */
+//@Configuration
+//@RestController
+//@EnableWebSecurity
+//public class SecurityAdapter {
+   
+  //  protected void configure(HttpSecurity http) throws Exception{
+     //   http.authorizeHttpRequests(
+              //  a -> a.antMatchers("/", "/error", "/webjars/**").permitAll().anyRequest().authenticated()
+              //  ).exceptionHandling(
+                //        e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.FORBIDDEN))
+         //   ).oauth2Login();//defaultSuccessUrl("/static/homePAge.html", true);
+         
+           // http.cors();and();http.csrf();disable();
+              
+        
+  //  }
+
+// }
